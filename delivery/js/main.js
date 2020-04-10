9b65e82122c1de9bef6a5f3fabb390531464d941
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2020-04-10 21:27:45 +0300

    fix to script and links

--- delivery/js/main.js
+++ delivery/js/main.js
@@ -14,9 +14,14 @@ function toggleModal() {
     modal.classList.toggle("is-open");
 }
 
+var delay = 0;
+
 document.querySelectorAll(".card").forEach(card => {
     card.classList.add("wow");
     card.classList.add("fadeInUp");
+    delay += 2;
+    card.classList.add('data-wow-delay="' + delay / 10 + 's"');
+    if (delay == 6) delay = 0;
 });
 
 // https://daneden.github.io/animate.css/   - анимации
